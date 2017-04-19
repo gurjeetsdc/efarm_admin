bb38525786c2172293638fc646f31db93cfc68ba
rohitk.kumar <rohitk.kumar@smartdatainc.net>
2017-04-19 18:50:15 +0530

    admin input section commit

--- src/app/input-management/add-input.component.ts
+++ src/app/input-management/add-input.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
+import { InputService } from './input.service';
+import { Router } from '@angular/router';
 
 @Component({
-  templateUrl: 'add-input.component.html'
+  templateUrl: 'add-input.component.html',
+  providers: [InputService]
 })
 export class AddInputComponent {
 	private input = {};
-  constructor() { }
+  constructor(private router : Router, private _inputService: InputService) { }
+
+  ngOnInit() { }
 
   addInput() {
-  	console.log("inside addInput",this.input)
-  }
+
+  	console.log("inside addInput",this.input);
+
+  	    this._inputService.inputadd(this.input)
+                       .subscribe(
+                           res => {
+                             console.log("response",res)
+                             this.router.navigate(['/input/list']);
+                           },
+                            err => {
+                            	console.log("error",err);
+                            });
+	}
+  
 }
--- src/app/input-management/input.service.ts
+++ src/app/input-management/input.service.ts
@@ -11,10 +11,22 @@ export class InputService {
   		console.log("inside inputlisting")
         let headers = new Headers();        
         let urlSearchParams = new URLSearchParams();
-        headers.append('Authorization', 'Bearer 67AgwREPU7naAECgGczTGgrMUt1MCst0');
+        headers.append('Authorization', 'Bearer FtvuJVQmkTaTVq4gDaL6h1y5tfZvGnQa');
         
         //let body = urlSearchParams.toString()
 		return this.http.get(this.host +'/inputs', { headers: headers }).map((res:Response) => res.json())
+    }
+
+
+    inputadd(inputs) {
+      console.log("inside input add");
+
+        let headers = new Headers();        
+        let urlSearchParams = new URLSearchParams();
+        headers.append('Authorization', 'Bearer FtvuJVQmkTaTVq4gDaL6h1y5tfZvGnQa');
+        
+        //let body = urlSearchParams.toString()
+    return this.http.post(this.host +'/inputs', inputs, { headers: headers }).map((res:Response) => res.json())
     }
 
 }
