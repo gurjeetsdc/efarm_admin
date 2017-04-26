6b6b6d233753c52285e16cf65d2903953fc4b69c
Manpreet Singh <manpreet.singh@smartdatainc.net>
2017-04-26 17:01:08 +0530

    changes in full layout for sub header and also in user and crop HTML

--- src/app/layouts/full-layout.component.ts
+++ src/app/layouts/full-layout.component.ts
@@ -1,20 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './full-layout.component.html'
 })
 export class FullLayoutComponent implements OnInit {
 
-  public disabled: boolean = false;
-  public status: {isopen: boolean} = {isopen: false};
-  public user:Object = {};
-  constructor(private router : Router ) { 
+  public disabled: boolean           = false;
+  public status: {isopen: boolean}   = {isopen: false};
+  public user:Object                 = {};
+  public active;
+  constructor(private router : Router, private _route: ActivatedRoute ) { 
     this.user = JSON.parse(localStorage.getItem("user"));
+    console.log("this._route.snapshot",this._route.snapshot["_urlSegment"].segments[0].path);
+    this.active = this._route.snapshot["_urlSegment"].segments[0].path;
   }
 
   public toggled(open: boolean): void {
-    console.log('Dropdown is now: ', open);
+    // console.log('Dropdown is now: ', open);
   }
 
   public toggleDropdown($event: MouseEvent): void {
@@ -25,8 +28,11 @@ export class FullLayoutComponent implements OnInit {
 
   ngOnInit(): void {}
 
+  layout(value) {
+    this.active = value;
+  }
+
   logout() {
-    console.log("logout")
     localStorage.removeItem('user');
     this.router.navigate(['/login']);
   }
