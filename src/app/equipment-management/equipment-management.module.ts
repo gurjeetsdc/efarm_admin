5088a6bd4d0c44e638a77720b7477b914bfe1859
Gurjeet Singh <gurjeets@smartdatainc.net>
2017-05-01 17:35:02 +0530

    Used Google Places.

--- src/app/equipment-management/equipment-management.module.ts
+++ src/app/equipment-management/equipment-management.module.ts
@@ -14,6 +14,7 @@ import { AddUpdateEquipmentComponent } from './addupdate-component/addupdate-equ
 
 import { EquipmentManagementRoutingModule } from './equipment-management-routing.module';
 
+import { GoogleplaceDirective } from '../shared/directives/googleplaces/googleplaces.directive';
 
 @NgModule({
     imports: [
@@ -25,7 +26,8 @@ import { EquipmentManagementRoutingModule } from './equipment-management-routing
     declarations: [
         ListEquipmentComponent,
         AddUpdateEquipmentComponent,
-        ViewEquipmentComponent           
+        ViewEquipmentComponent,
+        GoogleplaceDirective           
     ]
 })
 export class EquipmentManagementModule { }
