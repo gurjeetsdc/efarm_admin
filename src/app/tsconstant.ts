b738d6381c7375e38396678402e38d5db83689fb
Manpreet Singh <manpreet.singh@smartdatainc.net>
2017-05-04 14:14:30 +0530

    updated folder structure for users

--- src/app/tsconstant.ts
+++ src/app/tsconstant.ts
@@ -1,7 +1,5 @@
 'use strict';
 
 export const SEP='/';
-// export const HOST: string="https://efarmapi.herokuapp.com";
-// export const CLIENT_ID: string = '5x7EuN09HAeBn2pYJnvvq7szgJaULh14' 
-export const HOST: string="http://localhost:1337";
-export const CLIENT_ID: string = 'MQD5KWGwDtmglpQmmz1sVw6dWOJ68WD2'
+export const HOST: string="https://efarmapi.herokuapp.com";
+export const CLIENT_ID: string = '5x7EuN09HAeBn2pYJnvvq7szgJaULh14' 
