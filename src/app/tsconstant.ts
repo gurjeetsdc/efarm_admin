db86eb79725e37389d3ccf9c1d74989b55d548e0
KD <kamaldolikay@gmail.com>
2017-05-23 10:30:37 +0530

    first commit

--- src/app/tsconstant.ts
+++ src/app/tsconstant.ts
@@ -1,5 +1,6 @@
 'use strict';
 
 export const SEP='/';
-export const HOST: string="https://efarmapi.herokuapp.com";
+// export const HOST: string="https://efarmapi.herokuapp.com";
+export const HOST: string="http://localhost:1337";
 export const CLIENT_ID: string = '5x7EuN09HAeBn2pYJnvvq7szgJaULh14' 
