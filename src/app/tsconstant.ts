888bbfaf01ef2ff55b5d69462ff2c29cffe7a26c
rohitk.kumar <rohitk.kumar@smartdatainc.net>
2017-05-29 19:03:28 +0530

    Change for tsconstant file with staging API url

--- src/app/tsconstant.ts
+++ src/app/tsconstant.ts
@@ -1,5 +1,5 @@
 'use strict';
 
 export const SEP='/';
-export const HOST: string="https://efarmapi.herokuapp.com";
+export const HOST: string="http://52.39.212.226:4069";
 export const CLIENT_ID: string = '5x7EuN09HAeBn2pYJnvvq7szgJaULh14'
