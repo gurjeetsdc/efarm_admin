d2d53fde21cce66c68b6615ffed81d9ad9b1d616
Manpreet Singh <manpreet414@gmail.com>
2017-06-02 18:05:24 +0530

    added code in sub-heder for middleware handling

--- src/app/layouts/full-layout.component.ts
+++ src/app/layouts/full-layout.component.ts
@@ -1,19 +1,66 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-
+import { CommanService } from '../shared/services/comman.service';
 import { CookieService } from 'ngx-cookie';
 
 @Component({
   selector: 'app-dashboard',
-  templateUrl: './full-layout.component.html'
+  templateUrl: './full-layout.component.html',
+  providers:[CommanService]
 })
 export class FullLayoutComponent implements OnInit {
 
   public disabled: boolean           = false;
   public status: {isopen: boolean}   = {isopen: false};
   public active;
-  constructor(private router : Router, private _route: ActivatedRoute, private _cookieService: CookieService ) { 
-    this.active = this._route.snapshot["_urlSegment"].segments[0].path;
+  public access = {
+        crops:false,
+        inputs:false,
+        equipments:false,
+        lands:false,
+        users:false,
+        category:false,
+        manufacturer:false,
+        adminUsers:false,
+        adminRoles:false
+  }
+  
+  constructor(
+    private router : Router, 
+    private _route: ActivatedRoute, 
+    private _cookieService: CookieService,
+    private _commanService: CommanService ) { 
+        
+        this.active = this._route.snapshot["_urlSegment"].segments[0].path;
+          console.log('active',this.active);  
+        let actions = this._commanService.getActions();
+        if(actions["type"] == 'SA') {
+            this.access = {
+                crops:true,
+                inputs:true,
+                equipments:true,
+                lands:true,
+                users:true,
+                category:true,
+                manufacturer:true,
+                adminRoles:true,
+                adminUsers:true
+            }
+            console.log("SA",this.access)
+        } else {
+            this.access = {
+                crops:actions['crops'].view,
+                inputs:actions['inputs'].view,
+                equipments:actions['equipments'].view,
+                lands:actions['lands'].view,
+                users:actions['users'].view,
+                category:actions['category'].view,
+                manufacturer:actions['manufacturer'].view,
+                adminRoles:actions['adminRoles'].view,
+                adminUsers:actions['adminUsers'].view
+            }
+            console.log("not SA",this.access);
+        }
   }
 
   public toggled(open: boolean): void {
@@ -28,8 +75,34 @@ export class FullLayoutComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  layout(value) {
-    this.active = value;
+  layout(key) {
+      if(key == 'crops' && this.access.crops) {
+           this.applyRouter(key);
+      } else if(key == 'inputs' && this.access.inputs) {
+           this.applyRouter(key);
+      } else if(key == 'equipments' && this.access.equipments) {
+           this.applyRouter(key);
+      } else if(key == 'land' && this.access.lands) {
+           this.applyRouter(key);
+      } else if(key == 'users' && this.access.users) {
+           this.applyRouter(key);
+      } else if(key == 'category' && this.access.category) {
+           this.applyRouter(key);
+      } else if(key == 'manufacturer' && this.access.manufacturer) {
+           this.applyRouter(key);
+      } else if(key == 'roles' && this.access.adminRoles) {
+           this.applyRouter(key);
+      } else if(key == 'admin-users' && this.access.adminUsers) {
+           this.applyRouter(key);
+      } else {
+
+      }
+  }
+
+  applyRouter(key) {
+      this.active = key;
+      let route = '/' + key + '/list';
+      this.router.navigate([route]);
   }
 
   logout() {
