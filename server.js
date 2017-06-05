468c7d04dd522f2ddfd17f058c4a4c75e9e8552c
Manpreet Singh <manpreet.singh@smartdatainc.net>
2017-06-05 14:21:56 +0530

    changes in server.js and package file

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('*', (req, res) => {
 /**
  * Get port from environment and store in Express.
  */
-const port = process.env.PORT || '3000';
+const port = process.env.PORT || '4068';
 app.set('port', port);
 
 /**
